b7ff69aa08d18be746c004824b54d3281abdf9b2
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-09 00:20:44 +0430

    ProductController is added

---
+++ DDD/PresentationLayer/Controller/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controller
+{
+    public class ProductController : ApiController
+    {
+        public ProductController()
+        {
+            
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetProductById(Guid productId)
+        {
+            return Ok("GetProductById");
+        }
+
+        [HttpPost("Add")]
+        public IActionResult AddProduct()
+        {
+            return Ok("AddProduct");
+        }
+    }
+}
