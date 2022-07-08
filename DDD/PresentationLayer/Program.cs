5dafa00b060231e0ecff2924581e66ebbfd6993d
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-08 22:32:05 +0430

    Startup.cs and Swagger are added

--- DDD/PresentationLayer/Program.cs
+++ DDD/PresentationLayer/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using PresentationLayer;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebHost.CreateDefaultBuilder(args)
+    .UseStartup<Startup>();
+
 var app = builder.Build();
-
 
 app.Run();
---
+++ DDD/PresentationLayer/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace PresentationLayer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Title"
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors();
+            
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"{ (string.Empty) }/swagger/v1/swagger.json", "DDD Sample.API V1");
+                });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        
+    }
+}
