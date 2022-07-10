5678b1fa9bdd167627355a1d9daee7a408a878ca
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 00:15:34 +0430

    ProductRepository is used in the ProductCommandHandler

--- DDD/DomainLayer/DomainLayer/AggregatesModels/Products/CommandHandlers/ProductCommandHandler.cs
+++ DDD/DomainLayer/DomainLayer/AggregatesModels/Products/CommandHandlers/ProductCommandHandler.cs
@@ -1,13 +1,22 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using DomainLayer.DomainLayer.AggregatesModels.Products.Commands;
+using DomainLayer.DomainLayer.AggregatesModels.Products.Repository;
 using DomainLayer.DomainLayer.Exceptions;
+using DomainLayer.DomainLayer.Models;
 using MediatR;
 
 namespace DomainLayer.DomainLayer.AggregatesModels.Products.CommandHandlers
 {
     public class ProductCommandHandler : IRequestHandler<CreateProductCommand, bool>
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public async Task<bool> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
             if (!request.IsValid())
@@ -15,8 +24,9 @@ namespace DomainLayer.DomainLayer.AggregatesModels.Products.CommandHandlers
                 request.InlineValidationResult.Errors.ForEach(x => throw new CustomDomainException(x.ErrorMessage));
                 return false;
             }
-            
-            
+
+            var product = new Product(request.Name, request.Code, request.Quantity, request.Cost);
+            var existingProduct = _productRepository.FindByName(product);
 
             return true;
         }
