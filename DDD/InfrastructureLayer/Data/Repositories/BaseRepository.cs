2555ed98449c22e134c481df205d350a1d0ba466
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 00:14:34 +0430

    BaseRepository is added. But implementations have remained.

---
+++ DDD/InfrastructureLayer/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using DomainLayer.Core.Models;
+using DomainLayer.Core.Repository;
+using DomainLayer.Core.Specification;
+
+namespace InfrastructureLayer.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        public TEntity FindById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FindSingleBySpecification(ISpecification<TEntity> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(ISpecification<TEntity> specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
