de4afd1f104f51dadea5c24a6749aee727142086
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 00:14:58 +0430

    ProductRepository is added

---
+++ DDD/DomainLayer/DomainLayer/AggregatesModels/Products/Repository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using DomainLayer.Core.Repository;
+using DomainLayer.DomainLayer.Models;
+
+namespace DomainLayer.DomainLayer.AggregatesModels.Products.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Product FindByName(Product product);
+    }
+}
---
+++ DDD/InfrastructureLayer/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DomainLayer.DomainLayer.AggregatesModels.Products.Repository;
+using DomainLayer.DomainLayer.Models;
+
+namespace InfrastructureLayer.Data.Repositories
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public Product FindByName(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
