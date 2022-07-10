5b88145af273046260c65411c9e65ee924df3bd8
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 00:06:09 +0430

    Product domain model is added

---
+++ DDD/DomainLayer/DomainLayer/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DomainLayer.Core.Models;
+using DomainLayer.DomainLayer.Exceptions;
+
+namespace DomainLayer.DomainLayer.Models
+{
+    public class Product : Entity
+    {
+        public  string Name { get; protected set; }
+        public string Code { get; protected set; }
+        public int Quantity { get; protected set; }
+        public decimal Cost { get; protected set; }
+        public DateTime CreateDate { get; protected set; }
+        public DateTime ModifiedDate { get; protected set; }
+        public bool IsActive { get; protected set; }
+        public Product(string name, string code, int quantity, decimal cost)
+        {
+            if (string.IsNullOrEmpty(name)) throw new CustomDomainException(nameof(name));
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Code = code;
+            Quantity = quantity;
+            Cost = cost;
+            CreateDate = DateTime.Now;
+            ModifiedDate = DateTime.Now;
+            IsActive = true;
+        }
+    }
+}
