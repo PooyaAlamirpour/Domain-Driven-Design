3194dabe560df146bced17e21ef2a0b6396e32fe
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-10 13:40:35 +0430

    AddNewProductViewModel is added

---
+++ DDD/ApplicationLayer/Models/AddNewProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationLayer.Models
+{
+    /// <summary>
+    /// Adding new product
+    /// </summary>
+    public class AddNewProductViewModel
+    {
+        /// <summary>
+        /// Getting or setting the product name
+        /// </summary>
+        [Required(ErrorMessage = "Choose a name for product")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The product code is required")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Enter quantity of the product")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "The cost of product is required")]
+        public decimal Cost { get; set; }
+    }
+}
