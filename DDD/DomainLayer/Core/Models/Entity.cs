2179db849312ab4e72be3453e63feb28bacafa6e
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-10 23:55:24 +0430

    Entity is added

---
+++ DDD/DomainLayer/Core/Models/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using DomainLayer.Core.Events;
+
+namespace DomainLayer.Core.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; protected set; }
+        private List<DomainEvent> _domainEvents;
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        public List<DomainEvent> DomainEvents => _domainEvents;
+        
+        public void AddDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents ??= new List<DomainEvent>();
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
