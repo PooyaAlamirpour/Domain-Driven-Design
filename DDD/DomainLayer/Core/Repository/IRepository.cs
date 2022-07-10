ce0abc9f8be652d68dac4c25dfc1971ba0d560cf
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 00:05:30 +0430

    IRepository is added

---
+++ DDD/DomainLayer/Core/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using DomainLayer.Core.Models;
+using DomainLayer.Core.Specification;
+
+namespace DomainLayer.Core.Repository
+{
+    public interface IRepository<T> where T : Entity
+    {
+        T FindById(Guid id);
+        IEnumerable<T> Find();
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        T FindSingleBySpecification(ISpecification<T> specification);
+        IEnumerable<T> Find(ISpecification<T> specification);
+    }
+}
