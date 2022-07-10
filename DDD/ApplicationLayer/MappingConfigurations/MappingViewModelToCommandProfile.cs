098d0eaff3f18e1de717666f76411bc38f59827f
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-10 22:52:50 +0430

    MappingViewModelToCommandProfile is added

---
+++ DDD/ApplicationLayer/MappingConfigurations/MappingViewModelToCommandProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationLayer.Models;
+using AutoMapper;
+using DomainLayer.DomainLayer.AggregatesModels.Products.Commands;
+
+namespace ApplicationLayer.MappingConfigurations
+{
+    public class MappingViewModelToCommandProfile : Profile
+    {
+        public MappingViewModelToCommandProfile()
+        {
+            CreateMap<AddNewProductViewModel, CreateProductCommand>()
+                .ConstructUsing(x => new CreateProductCommand(x.Name, x.Code, x.Quantity, x.Cost));
+        }
+    }
+}
