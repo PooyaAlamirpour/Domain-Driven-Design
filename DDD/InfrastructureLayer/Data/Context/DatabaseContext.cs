c7b3e4c411c819e565fc7f11f31c9039dabb292c
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-11 23:57:27 +0430

    Database context is added

---
+++ DDD/InfrastructureLayer/Data/Context/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace InfrastructureLayer.Data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
