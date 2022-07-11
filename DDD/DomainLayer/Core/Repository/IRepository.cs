7e42308ebfb28e3ff0e496b771dc5e686c05f970
Pooya Alamirpour <pooya.alamirpour@gmail.com>
2022-07-12 00:12:19 +0430

    BaseRepository is added

--- DDD/DomainLayer/Core/Repository/IRepository.cs
+++ DDD/DomainLayer/Core/Repository/IRepository.cs
@@ -7,13 +7,13 @@ namespace DomainLayer.Core.Repository
 {
     public interface IRepository<T> where T : Entity
     {
-        T FindById(Guid id);
+        T? FindById(Guid id);
         IEnumerable<T> Find();
         T Add(T entity);
         void Update(T entity);
         void Delete(T entity);
 
-        T FindSingleBySpecification(ISpecification<T> specification);
-        IEnumerable<T> Find(ISpecification<T> specification);
+        T? FindSingleBySpecification(ISpecification<T> specification);
+        IEnumerable<T?> Find(ISpecification<T> specification);
     }
 }
--- DDD/InfrastructureLayer/Data/Repositories/BaseRepository.cs
+++ DDD/InfrastructureLayer/Data/Repositories/BaseRepository.cs
@@ -1,46 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DomainLayer.Core.Models;
 using DomainLayer.Core.Repository;
 using DomainLayer.Core.Specification;
+using InfrastructureLayer.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace InfrastructureLayer.Data.Repositories
 {
     public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
     {
-        public TEntity FindById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly DatabaseContext _dbContext;
 
-        public IEnumerable<TEntity> Find()
+        protected BaseRepository(DatabaseContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
         }
+        
+        public TEntity? FindById(Guid id) => _dbContext.Set<TEntity>().Find(id);
 
-        public TEntity Add(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<TEntity> Find() => _dbContext.Set<TEntity>().AsEnumerable();
+
+        public TEntity Add(TEntity entity) => _dbContext.Set<TEntity>().Add(entity).Entity;
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.Set<TEntity>().Update(entity);
         }
 
-        public void Delete(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
+        public void Delete(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
 
-        public TEntity FindSingleBySpecification(ISpecification<TEntity> specification)
-        {
-            throw new NotImplementedException();
-        }
+        public TEntity? FindSingleBySpecification(ISpecification<TEntity> specification) => Find(specification).FirstOrDefault();
 
-        public IEnumerable<TEntity> Find(ISpecification<TEntity> specification)
+        public IEnumerable<TEntity?> Find(ISpecification<TEntity> specification)
         {
-            throw new NotImplementedException();
+            var query = specification.Includes.Aggregate(_dbContext.Set<TEntity>().AsQueryable(),
+                (current, include) => current.Include(include));
+
+            var finalQuery = specification.IncludeStrings.Aggregate(query, 
+                (current, include) => current.Include(include));
+
+            return finalQuery.Where(specification.Criteria).AsEnumerable();
         }
     }
 }
